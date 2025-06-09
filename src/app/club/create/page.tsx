12b1def8afa0fc052cd09819946446948561933a
soomin <s2305@e-mirim.hs.kr>
2025-06-10 01:14:44 +0900

    [feat] 그룹 추가 페이지 마크업 작업

--- src/app/club/create/page.tsx
+++ src/app/club/create/page.tsx
@@ -53,34 +53,56 @@ export default function Page() {
                 )}
             </div>
 
-
-            {/* 이하 생략된 폼 */}
-            <p className="font-medium text-[24px] mt-[49px]">모임 정보</p>
             <div>
-                <input type="text" placeholder="모임 이름을 작성해주세요" />
-                <hr />
-                <p>호스트</p>
-                <input type="text" placeholder="이름을 작성해주세요" />
-                <div className="flex">
-                    <div>
-                        <p>장소</p>
-                        <input type="text" placeholder="이름을 작성해주세요" />
+                <p className="font-medium text-[24px] mt-[49px] mb-[21px]">모임 정보</p>
+                <div className="py-[28px] rounded-[10px] border border-[#CCC]">
+                    <div className="mx-[26px] flex flex-col gap-[18px] mb-[37px]">
+                        <div className="flex">
+                            <div>카테고리 추가</div>
+                        </div>
+                        <input
+                            type="text"
+                            placeholder="모임 이름을 작성해주세요"
+                            className="px-[15px] py-[13px] rounded-[10px] border border-[#CCC] bg-[#F5F5F5] text-[27px] font-semibold placeholder:text-black" />
                     </div>
-                    <div>
-                        <p>최대 인원</p>
-                        <input type="text" placeholder="이름을 작성해주세요" />
+                    <hr className="mx-[13px] mb-[23px]" />
+                    <div className="mx-[26px] flex flex-col gap-[12px] mb-[30px]">
+                        <p className="clubInputTitle">호스트</p>
+                        <input
+                            type="text"
+                            placeholder="이름을 작성해주세요"
+                            className="clubInputBox" />
+                    </div>
+                    <div className="mx-[26px] flex gap-[18px]">
+                        <div className="w-1/2 flex flex-col gap-[12px]">
+                            <p className="clubInputTitle">장소</p>
+                            <input
+                                type="text"
+                                placeholder="예. 광진구"
+                                className="w-full clubInputBox" />
+                        </div>
+                        <div className="w-1/2 flex flex-col gap-[12px]">
+                            <p className="clubInputTitle">최대 인원</p>
+                            <input
+                                type="text"
+                                placeholder="예. 15"
+                                className="w-full clubInputBox" />
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <p className="font-medium text-[24px] mt-[49px]">모임 정보</p>
-            <input
-                type="text"
-                placeholder="내용을 작성해주세요"
-                className="w-full h-[382px]"
-            />
-            <button type="button">취소하기</button>
-            <button type="submit">생성하기</button>
+            
+            <div>
+                <p className="font-medium text-[24px] mt-[49px] mb-[21px]">모임 소개</p>
+                <textarea
+                    placeholder="내용을 작성해주세요"
+                    className="w-full h-[382px] border border-[#CCC] rounded-[10px] px-[26px] py-[28px]"
+                ></textarea>
+            </div>
+            <div className="flex justify-end gap-[10px] mt-[57px] mb-[154px]">
+                <button type="button" className="button1">취소하기</button>
+                <button type="submit" className="button2">생성하기</button>
+            </div>
         </form>
     );
 }
