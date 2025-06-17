d1cac4ecf2a2c91c70ba3918bf0f420aac9c4088
soomin <s2305@e-mirim.hs.kr>
2025-06-18 04:11:46 +0900

    [feat] 로그인 연결 및 토스트 팝업 컴포넌트 구현

--- src/app/components/LoginCard.tsx
+++ src/app/components/LoginCard.tsx
@@ -1,9 +1,46 @@
+import { useState } from 'react';
+
 interface StepCardProps {
     onNext: () => void;
     onClose: () => void;
 }
 
 export default function LoginCard({ onNext, onClose }: StepCardProps) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                }),
+            });
+
+            // 테스트용 -> 나중에 삭제할 것
+            onClose();
+
+            if (!response.ok) {
+                alert('로그인 실패');
+                return;
+            }
+
+            const data = await response.json();
+            console.log('로그인 성공:', data);
+
+            onClose();
+        } catch (err) {
+            console.error('로그인 오류:', err);
+        }
+    };
+
     return (
         <div className="w-full h-full absolute z-20 px-[5%] overflow-hidden flex items-center backdrop-blur-[3px]"
             style={{ backgroundColor: 'rgba(2, 9, 17, 0.8)' }}>
@@ -11,17 +48,38 @@ export default function LoginCard({ onNext, onClose }: StepCardProps) {
                 <p className="font-semibold text-[20px] text-center mb-[40px]">로그인</p>
                 <div className="flex flex-col gap-[10px] mb-[24px]">
                     <p className="text-[18px] font-medium text-[#222]">아이디</p>
-                    <input type="text" className="w-full px-[20px] py-[17px] rounded-[8px] border border-[#CCC] bg-[#F0F0F0]"></input>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        className="w-full px-[20px] py-[17px] rounded-[8px] border border-[#CCC] bg-[#F0F0F0]"
+                    />
                 </div>
                 <div className="flex flex-col gap-[10px] mb-[30px]">
                     <p className="text-[18px] font-medium text-[#222]">비밀번호</p>
-                    <input type="text" className="w-full px-[20px] py-[17px] rounded-[8px] border border-[#CCC] bg-[#F0F0F0]"></input>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="w-full px-[20px] py-[17px] rounded-[8px] border border-[#CCC] bg-[#F0F0F0]"
+                    />
                 </div>
                 <div className="flex flex-col gap-[16px]">
-                    <button onClick={onClose} className="py-[17px] rounded-[8px] bg-[#276FE2] font-medium text-white text-[19px] w-full">로그인</button>
-                    <button onClick={onNext} className="py-[17px] rounded-[8px] font-medium text-[#276FE2] text-[19px] w-full border border-[#276FE2]">회원가입</button>
+                    <button
+                        onClick={handleLogin}
+                        className="py-[17px] rounded-[8px] bg-[#276FE2] font-medium text-white text-[19px] w-full"
+                    >
+                        로그인
+                    </button>
+                    <button
+                        type="button"
+                        onClick={onNext}
+                        className="py-[17px] rounded-[8px] font-medium text-[#276FE2] text-[19px] w-full border border-[#276FE2]"
+                    >
+                        회원가입
+                    </button>
                 </div>
             </form>
         </div>
-    )
+    );
 }
